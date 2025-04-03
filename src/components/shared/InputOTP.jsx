4b5e310340814646c0cd84d9568278a687b7e991
Vinoth S <vinoth.s@vhitechlab.com>
2025-04-03 13:17:54 +0530

    after paste handle

--- src/components/shared/InputOTP.jsx
+++ src/components/shared/InputOTP.jsx
@@ -11,13 +11,29 @@ const InputOTP = ({ length = 6, type = "number" }) => {
   const [otp, setOtp] = React.useState(Array(length).fill(""));
   const optpRef = React.useRef([]);
 
+  const moveCusorToEnd = (index) => {
+    requestAnimationFrame(() => {
+      optpRef.current[index - 1].setSelectionRange(
+        optpRef.current[index - 1].value.length,
+        optpRef.current[index - 1].value.length
+      );
+    });
+  };
+
+  const handleFocus = (index) => {
+    requestAnimationFrame(() => {
+      optpRef.current[index].focus();
+    });
+  };
+
   const handleKeyDown = (e, index) => {
     if (e.key === "Backspace" && !otp[index] && index > 0) {
       optpRef.current[index - 1].focus();
     } else if (e.key === "ArrowLeft" && index > 0) {
       optpRef.current[index - 1].focus();
+      moveCusorToEnd(index);
     } else if (e.key === "ArrowRight" && index < length - 1) {
-      optpRef.current[index + 1].focus();
+      handleFocus(index + 1);
     }
   };
 
@@ -32,8 +48,18 @@ const InputOTP = ({ length = 6, type = "number" }) => {
       return newOtp;
     });
     if (e.target.value && index < length - 1) {
-      optpRef.current[index + 1].focus();
+      handleFocus(index + 1);
     }
+  };
+
+  const handlePaste = (e) => {
+    e.preventDefault();
+    const pastedData = e.clipboardData.getData("text");
+    if (!regex[type].test(pastedData)) return;
+    setOtp(otp.map((_, index) => pastedData[index] || ""));
+    handleFocus(
+      pastedData.length < length ? pastedData.length - 1 : length - 1
+    );
   };
 
   useEffect(() => {
@@ -51,6 +77,8 @@ const InputOTP = ({ length = 6, type = "number" }) => {
           ref={(input) => (optpRef.current[index] = input)}
           onChange={(e) => handleChange(e, index)}
           onKeyDown={(e) => handleKeyDown(e, index)}
+          disabled={index !== 0 && !otp[index - 1] && !value}
+          onPaste={handlePaste}
         />
       ))}
     </div>
