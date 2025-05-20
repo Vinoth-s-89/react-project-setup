c7e5b924ceecc7ea85850f93d8827ea0c52a3dde
Vinoth S <vinoth.s@vhitechlab.com>
2025-05-20 19:53:35 +0530

    before files and folders

--- src/components/shared/FileExplorer.jsx
+++ src/components/shared/FileExplorer.jsx
@@ -1,8 +1,39 @@
+import { useState } from "react";
 import { icons } from "../../constants/icons";
 import "../../styles/fileexplorer.css";
 
 const FileExplorer = () => {
-  return <div>FileExplorer {icons["arrow-right"]}</div>;
+  const [isExpanded, setIsExpanded] = useState(false);
+  return (
+    <div className="app-container">
+      <div
+        className="explorer-container"
+        style={{ gridTemplateRows: isExpanded ? "auto 1fr" : "auto 0fr" }}
+      >
+        <div
+          className="parent-container"
+          onClick={() => setIsExpanded(!isExpanded)}
+        >
+          <div
+            className="expand-icon"
+            style={{ rotate: isExpanded ? "90deg" : "0deg" }}
+          >
+            {icons["arrow-right"]}
+          </div>
+          <div className="parent-folder-name">
+            File Explorer File Explorer File Explorer File Explorer
+          </div>
+          <div className="icons-container">
+            {icons.newfile}
+            {icons.newfolder}
+            {icons.refresh}
+            {icons.minimize}
+          </div>
+        </div>
+        <div className="files-and-folders"></div>
+      </div>
+    </div>
+  );
 };
 
 export default FileExplorer;
